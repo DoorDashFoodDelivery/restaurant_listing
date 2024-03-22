37d0ba26b0133e336d44eb24f5441eedea3d0d6a
dhananjay <waradedhananjay2000@gmail.com>
2024-03-22 15:31:30 +0530

    add a business logic in Restaurant service how we convert restaurant to restaurantDTO in the form of list

--- src/main/java/com/example/demo/service/RestaurantService.java
+++ src/main/java/com/example/demo/service/RestaurantService.java
@@ -1,12 +1,14 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.RestaurantDTO;
 import com.example.demo.entity.Restaurant;
+import com.example.demo.mapper.RestaurantMapper;
 import com.example.demo.repo.RestaurantRepo;
 
 @Service
@@ -16,9 +18,11 @@ public class RestaurantService {
 	RestaurantRepo restaurantRepo;
 
 	public List<RestaurantDTO> findAllRestaurant() {
-		List<Restaurant>restaurant=restaurantRepo.findAll();
+		List<Restaurant>restaurants=restaurantRepo.findAll();
 		// map it to list of Dtos
-		return null;
+		
+		List<RestaurantDTO>restaurantDTOList=restaurants.stream().map(restaurant->RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant)).collect(Collectors.toList());
+		return restaurantDTOList;
 	}
 	
 }
