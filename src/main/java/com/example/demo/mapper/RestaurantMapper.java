8007e52911cbf991c84ef332cdf3d517e7562890
dhananjay <waradedhananjay2000@gmail.com>
2024-03-22 15:21:53 +0530

    map restaurantDTO To Restaurant and restaurant to restaurantDTO

--- src/main/java/com/example/demo/mapper/RestaurantMapper.java
+++ src/main/java/com/example/demo/mapper/RestaurantMapper.java
@@ -1,5 +1,19 @@
 package com.example.demo.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.example.demo.dto.RestaurantDTO;
+import com.example.demo.entity.Restaurant;
+
+@Mapper
 public interface RestaurantMapper {
 
+	RestaurantMapper INSTANCE=Mappers.getMapper(RestaurantMapper.class);
+	
+	Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);	
+	
+	RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);	
+
+	
 }
